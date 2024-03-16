23cd7720feeccb4edacd6b8bdb9391111b3a0a45
Leandro Heilig <leandrohpassos@gmail.com>
2024-03-16 17:07:19 -0300

    feat: create middleware

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,10 +4,12 @@ import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
 import { DetailUserController } from './controllers/user/DetailUserController'
 
+import { isAuthenticated } from './middlewares/isAuthenticated'
+
 const router = Router()
 
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
-router.get('/user', new DetailUserController().handle)
+router.get('/user', isAuthenticated ,new DetailUserController().handle)
 
 export  {router}
