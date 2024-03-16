b3b8e43f9d5cbf3716976627f4c5ab8dc08254c5
Leandro Heilig <leandrohpassos@gmail.com>
2024-03-16 15:54:46 -0300

    feat: add DetailUserController and DetailUserService

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express'
 
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
+import { DetailUserController } from './controllers/user/DetailUserController'
 
 const router = Router()
 
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
+router.get('/user', new DetailUserController().handle)
 
 export  {router}
---
+++ backend/src/services/users/DetailUserService.ts
@@ -0,0 +1,9 @@
+import PrismaClient from '../../prisma'
+
+class DetailUserService { 
+  async execute() {
+    return { ok: true } 
+  }
+}
+
+export { DetailUserService } 
