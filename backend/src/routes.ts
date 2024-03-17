f2f221da98571f453b97e0482ed410f212a11654
Leandro Heilig <leandrohpassos@gmail.com>
2024-03-17 20:02:55 -0300

    feat: add category services creation

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,11 +5,16 @@ import { AuthUserController } from './controllers/user/AuthUserController'
 import { DetailUserController } from './controllers/user/DetailUserController'
 
 import { isAuthenticated } from './middlewares/isAuthenticated'
+import { CreateCategoryController } from './controllers/category/CreateCategoryController'
 
 const router = Router()
 
+// Rotas do usuário
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
-router.get('/user', isAuthenticated ,new DetailUserController().handle)
+router.get('/user', isAuthenticated, new DetailUserController().handle)
+
+// Rotas de Categorias
+router.post('/categories', isAuthenticated, new CreateCategoryController().handle)
 
 export  {router}
