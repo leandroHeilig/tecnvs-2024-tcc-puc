f99ca5b8b3eb45279b0d18418eef5bdfc2a42de5
Leandro Heilig <leandrohpassos@gmail.com>
2024-04-24 20:43:31 -0300

    chore: clean default tags and create another page

---
+++ front/src/pages/newsletter.tsx
@@ -0,0 +1,6 @@
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+
+export default function Newsletter() {
+  return <div className={styles.main}>Newsletter</div>;
+}
