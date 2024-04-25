aec3ddcee618ea60a5b7bc809f01864393b241c0
Leandro Heilig <leandrohpassos@gmail.com>
2024-04-24 21:30:08 -0300

    chore: setting scss and rules

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,13 +1,8 @@
 
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
     <>
-      <div className={styles.main}>
+      <div>
         <h1>TCC PUC MG</h1>
 
       </div>     
--- front/src/pages/newsletter.tsx
+++ front/src/pages/newsletter.tsx
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import styles from "@/styles/Home.module.scss";
 
 export default function Newsletter() {
   return <div className={styles.main}>Newsletter</div>;
