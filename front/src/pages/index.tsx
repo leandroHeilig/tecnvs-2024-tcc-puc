1f5ed083d768d6085300def1ecbd4b6a147c11f0
Leandro Heilig <leandrohpassos@gmail.com>
2024-04-24 22:00:43 -0300

    feat: add tag title and test for index

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,10 +1,13 @@
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Portal do Analista</title>
+      </Head>
       <div>
         <h1>TCC PUC MG</h1>
-
       </div>     
     </>
   );
