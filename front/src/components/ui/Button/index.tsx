aa2616cb63f21991e46442c6b001896a977a44d3
Leandro Heilig <leandrohpassos@gmail.com>
2024-04-27 17:58:38 -0300

    feat: create ui componentes for login page

---
+++ front/src/components/ui/Button/index.tsx
@@ -0,0 +1,20 @@
+import { ReactNode, ButtonHTMLAttributes } from 'react'
+import { FaSpinner } from 'react-icons/fa'
+import styles from './styles.module.scss'
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  loading?: boolean;
+  children: ReactNode;
+}
+
+export function Button({ children, loading, ...rest }: ButtonProps) {
+  return (
+    <button className={styles.button} disabled={loading} {...rest}>
+      {loading ? (
+        <FaSpinner />
+      ) : (
+        <a className={styles.buttonText}>{children}</a>
+      )}
+    </button>
+  );
+}
--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,4 +1,11 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import styles from '@/styles/Home.module.scss'
+
+import logoImg from '../../public/logo-2-branco.png'
+
+import { Input } from '@/components/ui/Input'
+import { Button } from '@/components/ui/Button'
 
 export default function Home() {
   return (
@@ -6,9 +13,19 @@ export default function Home() {
       <Head>
         <title>Portal do Analista</title>
       </Head>
-      <div>
-        <h1>TCC PUC MG</h1>
-      </div>     
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo site" />
+        <div className={styles.login}>
+          <Input placeholder="Digite o seu e-mail" type='text' />
+          <Input placeholder="Informa a sua senha" type='password' />
+          <Button
+            type='submit'
+            loading={false}
+          >
+            Login
+          </Button>
+        </div>
+      </div>
     </>
   );
 }
