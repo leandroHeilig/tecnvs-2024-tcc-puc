206aecaed42ff3bb13b646770cccd11f04b2d1ff
Leandro Heilig <leandrohpassos@gmail.com>
2024-04-27 21:41:53 -0300

    feat: create authcontext

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useContext, FormEvent } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -9,7 +10,22 @@ import logoImg from '../../public/logo-2-branco.png'
 import { Input } from '@/components/ui/Input'
 import { Button } from '@/components/ui/Button'
 
+import { AuthContext } from '@/contexts/AuthContext'
+
 export default function Home() {
+  const { signIn } = useContext(AuthContext)
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault()
+    
+    let data = {
+      email: 'test@teste.com.br',
+      password:'123456'
+    }
+
+    await signIn(data);
+  }
+
   return (
     <>
       <Head>
@@ -19,7 +35,7 @@ export default function Home() {
         <Image src={logoImg} alt="Logo site" />
         <div className={styles.login}>
           <h1>Portal do Analista</h1>
-          <form>
+          <form onSubmit={handleLogin}>
             <Input placeholder="Digite o seu e-mail" type="text" />
             <Input placeholder="Informa a sua senha" type="password" />
             <Button type="submit" loading={false}>
