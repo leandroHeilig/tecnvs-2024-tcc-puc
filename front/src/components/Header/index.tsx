90feae0a14e2fd11aab15ecf3407eab61b902b5a
Leandro Heilig <leandrohpassos@gmail.com>
2024-05-04 16:39:22 -0300

    feat: add header component

---
+++ front/src/components/Header/index.tsx
@@ -0,0 +1,39 @@
+import styles from './styles.module.scss'
+
+import { useContext } from 'react'
+import Link from 'next/link'
+import { FiLogOut } from 'react-icons/fi'
+
+import { AuthContext } from '@/contexts/AuthContext'
+
+export function Header() {
+
+  const { signOut } = useContext(AuthContext)
+  
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Link href="/">
+          <img src="/logo-2-branco.png" alt="logo" />
+        </Link>
+        <nav className={styles.menuNav}>
+          <Link href="/category" legacyBehavior>
+            <a href="">Categoria</a>
+          </Link>
+
+          <Link href="/services" legacyBehavior>
+            <a href="">Serviços</a>
+          </Link>
+
+          <Link href="/appointments" legacyBehavior>
+            <a href="">Apontamentos</a>
+          </Link>
+
+          <button onClick={signOut}>
+            <FiLogOut color="#fff" />
+          </button>
+        </nav>
+      </div>
+    </header>
+  );
+}
--- front/src/pages/dashboard/index.tsx
+++ front/src/pages/dashboard/index.tsx
@@ -1,10 +1,19 @@
 import { canSSRAuth } from '@/utils/canSSRAuth'
+import Head from 'next/head'
+
+import { Header } from '@/components/Header'
 
 export default function Dashboard() {
-  return (
-    <div>
-      <h1>Painel</h1>
-    </div>
+  return (    
+    <>
+      <Head>
+        <title>Tecnvs - Dashboard</title>
+      </Head>
+      <div>
+        <Header/>
+        <h1>Dashboard</h1>
+      </div>
+    </>
   )  
 }
 
