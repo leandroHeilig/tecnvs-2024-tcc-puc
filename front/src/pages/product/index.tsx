98d858a51eecc565b894097e109c031426571e67
Leandro Heilig <leandrohpassos@gmail.com>
2024-05-08 22:04:57 -0300

    feat: add fields to services page

--- front/src/pages/product/index.tsx
+++ front/src/pages/product/index.tsx
@@ -5,7 +5,9 @@ import { canSSRAuth } from '@/utils/canSSRAuth'
 import { Header } from '@/components/Header'
 
 import { setupAPIClient } from '@/services/api'
-import { useState } from 'react'
+import { useState, FormEvent, ChangeEvent } from 'react'
+
+import { toast } from 'react-toastify'
 
 type ItemProps = {
   id: string
@@ -21,8 +23,41 @@ export default function Product({ categoryList }: CategoryProps) {
   const [categories, setCategories] = useState(categoryList || []);
   const [categorySelected, setCategorySelected] = useState(0)
 
+  const [ name, setName ] = useState('')
+  const [ price, setPrice ] = useState("")
+
   function handleChangeCategory(event) {
     setCategorySelected(event.target.value);
+  }
+
+  async function handleRegister(event: FormEvent) {
+    event.preventDefault()
+
+    try {
+
+      if (name === ' ' || price === '') {
+        toast.warning('Preencha todos os campos')
+        return
+      }
+
+      const apiClient = setupAPIClient();
+      await apiClient.post("/product", {
+        description: name,
+        price: price,
+        category_id: categories[categorySelected].id,
+      });
+
+      toast.success("Produto cadastrado com sucesso");
+      
+      
+    } catch (error) {
+      console.log(error)
+      toast.error('Falha ao cadastrar o registro.')
+    }
+
+    setName("");
+    setPrice("");
+    
   }
 
   return (
@@ -34,7 +69,7 @@ export default function Product({ categoryList }: CategoryProps) {
         <Header />
         <main className={styles.container}>
           <h1>Novo Produto ou Serviço</h1>
-          <form className={styles.form}>
+          <form className={styles.form} onSubmit={handleRegister}>
             <select value={categorySelected} onChange={handleChangeCategory}>
               {categories.map((item, index) => {
                 return (
@@ -47,12 +82,18 @@ export default function Product({ categoryList }: CategoryProps) {
             <input
               type="text"
               className={styles.input}
-              placeholder="Digite a descição do Produto"
+              placeholder="Digite o nome do Produto / Serviço"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
+
+
             <input
               type="text"
               className={styles.input}
               placeholder="Digite o preço do Produto"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
             />
             <button className={styles.buttonAdd} type="submit">
               Cadastrar
